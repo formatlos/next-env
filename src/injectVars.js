e4b04c1ba95a684c4af32b6c1edcca22858b996e
Martin Raedlinger <mr@formatlos.de>
2018-08-17 20:07:10 +0200

    remove fat-arrow usage

--- src/injectVars.js
+++ src/injectVars.js
@@ -3,7 +3,7 @@ if (window && window.__NEXT_DATA__ && window.__NEXT_DATA__.runtimeConfig) {
   if (!process) process = {};
   if (!process.env) process.env = {};
 
-  Object.keys(publicRuntimeConfig).forEach(key => {
+  Object.keys(publicRuntimeConfig).forEach(function(key) {
     process.env[key] = publicRuntimeConfig[key];
   });
 }
