5379c764c4db26ea9b506f9653bf751bdf7a3b40
Martin Raedlinger <mr@formatlos.de>
2019-02-18 15:14:59 +0100

    add support for next v8

--- src/injectVars.js
+++ src/injectVars.js
@@ -1,9 +1,22 @@
-if (window && window.__NEXT_DATA__ && window.__NEXT_DATA__.runtimeConfig) {
-  var publicRuntimeConfig = window.__NEXT_DATA__.runtimeConfig;
-  if (!process) process = {};
-  if (!process.env) process.env = {};
+if(window) {
+  // next < 8 stores __NEXT_DATA__ in a js variable already
+  var nextData = window.__NEXT_DATA__;
+  // since next 8 __NEXT_DATA__ is stored as json
+  var nextDataScript = document.getElementById('__NEXT_DATA__');
+  if(nextDataScript) {
+    nextData = JSON.parse(nextDataScript.textContent);
+  }
+  if (nextData && nextData.runtimeConfig) {
+    var publicRuntimeConfig = nextData.runtimeConfig;
+    if (!process) process = {};
+    if (!process.env) process.env = {};
 
-  Object.keys(publicRuntimeConfig).forEach(function(key) {
-    process.env[key] = publicRuntimeConfig[key];
-  });
+    Object.keys(publicRuntimeConfig).forEach(function(key) {
+      process.env[key] = publicRuntimeConfig[key];
+    });
+  }
 }
+
+
+
+
